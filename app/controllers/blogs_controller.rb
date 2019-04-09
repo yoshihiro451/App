49be674307a81fa2bae09065328e66a5e00dcaf2
yoshihiro yokoi <yoshiair@MacBook-Air-yoshi.local>
2019-04-09 20:03:06 +0900

    finish 4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,11 +6,24 @@ class BlogsController < ApplicationController
   end
 
   def new
+  	@blog = Blog.new
   end
 
   def create
   end
 
   def edit
+  end
+
+  def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
+  end
+
+  private
+
+  def blog_params
+  	paras.require(:blog).permit(:title, :category, :body)
   end
 end
